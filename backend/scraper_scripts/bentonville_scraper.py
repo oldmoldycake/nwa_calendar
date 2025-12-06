c86668201fd7cf1ce4ae880846d224aa986fa48c
OldMoldyCake <nolan.ruger@gmail.com>
2025-12-06 09:05:17 -0600

    Work on rogers scraper

--- backend/scraper_scripts/bentonville_scraper.py
+++ backend/scraper_scripts/bentonville_scraper.py
@@ -7,6 +7,8 @@ import time
 import os
 import datetime
 
+from selenium.webdriver.common import options
+
 
 db_path = os.path.expandvars("/home/oldmoldycake/Projects/nwa_calendar/backend/events.db")
 conn = sqlite3.connect(db_path)
@@ -42,7 +44,7 @@ total_events = parts[1]
 
 event_links = []
 count = 0
-while len(event_links) < int(total_events):
+while len(event_links) < 12:
     page_events_tags = soup.find_all('div', class_="item")
     for page_event_tag in page_events_tags:
         header_tag = page_event_tag.find('h4')
@@ -52,9 +54,7 @@ while len(event_links) < int(total_events):
         header_link_tag = header_tag.find('a')
         header_link = "https://visitbentonville.com" + str(header_link_tag.get("href"))
         event_links.append(header_link) 
-        print(header_link) 
     next_page_url = f"https://www.visitbentonville.com/events/?skip={len(event_links)}&bounds=false&view=list&sort=date&filter_daterange%5Bstart%5D={todays_date}&filter_daterange%5Bend%5D={formatted_one_year_date}"
-    print(next_page_url)
     
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(next_page_url)
@@ -62,5 +62,75 @@ while len(event_links) < int(total_events):
     full_html = driver.page_source
     driver.quit()
     soup = BeautifulSoup(full_html, 'lxml')
+
+
+for event_link in event_links:
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(event_link)
+    time.sleep(1)
+    full_html = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(full_html, 'lxml')
+    
+    #Get Description
+    description_container_tag = soup.find('div', class_="core-styles")
+    description = description_container_tag.get_text(strip=True)
+      
+    
+    info_container = soup.find('div', class_='info-section')
+    
+    header = info_container.find('h1')
+    event_title = header.get_text(strip=True)
+    
+    #Gate dates
+    date_info = soup.find('dl', class_ = "priority-info")
+    date_label = date_info.find('dt', string='Dates:')
+    date_text = date_label.find_next_sibling('dd').get_text(strip=True)
+    dates = date_text.split(' - ')
+
+    if len(dates) == 2:
+        start_date, end_date = dates
+    else:
+        start_date, end_date = [dates, None]
+
+    date_fornmat ="%B %d, %Y"
+
+    formatted_start_date = datetime.datetime.strptime(start_date.strip(), date_fornmat)
+    formatted_end_date = datetime.datetime.strptime(end_date.strip(), date_fornmat)
+    
+    formatted_start_date = formatted_start_date.strftime("%Y-%m-%d")
+    formatted_end_date = formatted_end_date.strftime("%Y-%m-%d")
+   
+    #Get the location
+    location_container = soup.find('div', class_="two-line-wrap")
+    location_tags = location_container.find_all('span')
+    
+    location = location_tags[0].get_text() + " " + location_tags[1].get_text()
+    
+    record_event_sql = f"""
+    INSERT INTO events (event_link, event_title, event_description, location, datetime_start, datetime_end
+    VALUES(?,?,?,?,?,?)
+    """
+
+    insert_event_sql = (
+            event_link,
+            event_title,
+            description,
+            location,
+            start_date,
+            end_date if end_date is not None else start_date
+            )
+    cursor.execute(record_event_sql, insert_event_sql)
+    conn.commit()
+            
+     
+    
+
+    
+
+    print("\n\n")
+
+
+
 
 
